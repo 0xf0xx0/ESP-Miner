a272dc121e0e9fd81ca5b6042f73a28f3412e064
giing <33764485+0xf0xx0@users.noreply.github.com>
2026-08-21 12:28:52 -0400

    remove SYSTEM_notify_new_ntime

--- main/main.c
+++ main/main.c
@@ -218,7 +218,10 @@ void app_main(void)
 
     int retry = 15;
     while (esp_netif_sntp_sync_wait(2000 / portTICK_PERIOD_MS) == ESP_ERR_TIMEOUT && --retry >= 0) {
-        ESP_LOGI(TAG, "Waiting for system time to be set... (%d attempts remaining)", retry);
+        ESP_LOGI(TAG, "Waiting for NTP... (%d attempts remaining)", retry);
+    }
+    if (retry == -1) {
+        ESP_LOGW(TAG, "Failed to get NTP time! Certificate validation may fail!");
     }
 
     miner_job_pool_init();
